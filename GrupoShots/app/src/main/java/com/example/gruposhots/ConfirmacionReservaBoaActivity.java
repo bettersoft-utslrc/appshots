9581b18d4c124991e1cbd0008b8b4fb8103245f7
paolopayan08 <paolopayan08@gmail.com>
2019-07-24 18:14:53 -0500

    paso de parametros y diseño a la pantalla de reservacion

--- GrupoShots/app/src/main/java/com/example/gruposhots/ConfirmacionReservaBoaActivity.java
+++ GrupoShots/app/src/main/java/com/example/gruposhots/ConfirmacionReservaBoaActivity.java
@@ -1,19 +1,58 @@
 package com.example.gruposhots;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ConfirmacionReservaBoaActivity extends AppCompatActivity {
     private TextView Fecha;
     private TextView Salon;
     private TextView Vip;
     private TextView Mesa;
+    private TextView Nombre;
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_confirmacion_reserva_boa);
+
+        database = FirebaseDatabase.getInstance ();
+        user = FirebaseAuth.getInstance ().getCurrentUser ();
+        reference = database.getReference ("Usuarios").child (user.getUid ());
+
+        Nombre = (TextView) findViewById (R.id.txtnombreResiver);
+
+        reference.child (user.getUid ()).addValueEventListener (new ValueEventListener () {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.exists ()){
+                        String  nombre = (String) dataSnapshot.child ("Nombre").getValue ();
+                        Nombre.setText ("Nombre: " + nombre);
+                    }else {
+                        Toast.makeText (ConfirmacionReservaBoaActivity.this, "valiste verga", Toast.LENGTH_LONG).show ();
+                    }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
 
         Fecha = (TextView) findViewById (R.id.txtFechaResiver);
         String fecha = getIntent ().getStringExtra ("fecha");
